8b6d70b8895c8d8fba4d201d7cdd32b194805cd1
eai <alexander.enikeev@gmail.com>
2020-02-24 18:16:59 +0300

    complete task 4. Change profile.html and render_profile_teacher function

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, url_for, redirect
+import json
+import pprint
 from data import goals, teachers
 
 app = Flask(__name__)
+
+with open ('teachers_data.json', 'w') as f:
+    json.dump(teachers, f)
+
 
 
 @app.route('/')
@@ -18,8 +24,27 @@ def render_goals(goal):
 
 @app.route('/profiles/<id_teacher>')
 def render_profile_teacher(id_teacher='1'):
-    return "Здесь будет преподаватель <id_teacher>"
-    # return render_template()
+    teacher_id = int(id_teacher)
+    free_time_counter = 0
+    bisy_days = []
+    with open("teachers_data.json", "r") as f:
+        teachers_json = json.load(f)
+    for teacher in teachers_json:
+        if teacher['id'] == teacher_id:
+            teacher_json = teacher
+    for day in teacher_json['free']:
+        for time in teacher_json['free'][day]:
+            if teacher_json['free'][day][time] == True:
+                free_time_counter += 1
+        if free_time_counter == 0:
+            bisy_days.append(day)
+        free_time_counter = 0
+    pprint.pprint(bisy_days)
+    return render_template('profile.html', teacher = teacher_json, bisy_days=bisy_days, goals=goals)
+
+'''{% if True not in teacher.free %}
+                                    
+                                {% endif %}'''
 
 
 @app.route('/request/')
@@ -35,7 +60,7 @@ def render_request_done():
 
 
 @app.route('/booking/<id_teacher>/<day>/<time>/')
-def render_booking():
+def render_booking(id_teacher, day, time):
     return "Здесь будет форма бронирования <id_teacher>"
     # return render_template()
 
